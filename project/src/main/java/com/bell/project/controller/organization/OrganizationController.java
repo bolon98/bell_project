4ce7753cedb10fbfdca66e514b90564532dae657
Dmitry <bolon98@mail.ru>
2019-05-06 15:02:11 +0400

    помник

---
+++ project/src/main/java/com/bell/project/controller/organization/OrganizationController.java
@@ -0,0 +1,12 @@
+package com.bell.project.controller.organization;
+
+//import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Api(value = "OrganizationController", descriptor = "Управление информацией об организациях")
+@RestController
+@RequestMapping(value = "/organization")
+public class OrganizationController {
+
+}
